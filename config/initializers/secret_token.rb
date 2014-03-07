17c8bbbb22e7c7f83478f1093d781241a8b0e809
Justin Huang <justingotemail@gmail.com>
2014-03-07 03:35:21 -0500

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bitcharity::Application.config.secret_key_base = 'a6450ee6e34ce8ec59be93e09696b92e9dd885a8ac6f14b74b0b213402bf848a5af4b6435b2fb4b6ece0b0cb1a6fff951c5d7469b591b924f752c48dcc10d046'
