09456e349589836a88aef59cc779fb7736718e7d
Justin Huang <justingotemail@gmail.com>
2014-03-16 02:59:31 -0400

    print user object

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -7,7 +7,7 @@ class DashboardController < ApplicationController
     @charities = Charity.all
 
     @donations = Transaction.where email: current_user.email
-    puts @donations
+    puts @user.inspect
   end
 
   def selectPrimary
